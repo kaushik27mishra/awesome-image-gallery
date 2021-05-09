d10d8fd19b12c0f01137b7a2cbfb6c25e456ce94
Kaushik Mishra <kmishra27.km@gmail.com>
2021-05-09 14:21:09 +0530

    infinite scroll logic setup

--- src/page/Results.js
+++ src/page/Results.js
@@ -1,30 +1,50 @@
 import React, { useEffect, useState } from "react";
-import { API_KEY } from "../utils/APIclientHome";
 import axios from "axios";
+import _ from "lodash";
+import { API_KEY } from "../utils/APIclientHome";
 import { getPhotos } from "../utils/getPhotos";
 
 function Results(props) {
-  // const [query, setQuery] = useState("");
+  // const [query, getSearc] = useState("");
   const [images, setImages] = useState([]);
+  const [page, setPage] = useState(1);
+  const queryString = props.location.search.slice(3);
 
   useEffect(() => {
-    const query = props.location.search.slice(3);
-    let mounted = true;
     axios
       .get(
-        `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${API_KEY}&text=${query}&per_page=20&format=json&nojsoncallback=1`
+        `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${API_KEY}&text=${queryString}&per_page=20&page=${page}&safe_search=1&content_type=1&format=json&nojsoncallback=1`
       )
       .then((res) => {
-        if (mounted) {
-          // console.log(res);
-          const imagesArray = getPhotos(res);
-          setImages(imagesArray);
-        }
+        console.log("API Called", page);
+        const imagesArray = getPhotos(res);
+        setImages((old) => [...old, ...imagesArray]);
       });
-    return () => (mounted = false);
-  }, []);
+  }, [page]);
 
-  return <h1>Results</h1>;
+  window.onscroll = _.debounce(() => {
+    const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+
+    if (clientHeight + scrollTop >= scrollHeight - 5) {
+      setPage((page) => page + 1);
+    }
+  }, 1000);
+
+  return (
+    <>
+      <section id="results">
+        <div></div>
+        {images.map((photo, i) => {
+          return (
+            <div key={i}>
+              <img src={photo} style={{ height: 384, width: 512 }} />
+            </div>
+          );
+        })}
+        <div></div>
+      </section>
+    </>
+  );
 }
 
 export default Results;
